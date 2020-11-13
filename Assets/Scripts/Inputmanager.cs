c3e62ba804e9ffbc071e4b6930d2275f7f1f0eb3
Lyretto <Lyretto>
2020-11-13 10:37:44 +0100

    improved player movement

--- Assets/Scripts/Inputmanager.cs
+++ Assets/Scripts/Inputmanager.cs
@@ -8,9 +8,6 @@ public class Inputmanager : MonoBehaviour
 
     public float mouseDetect = 10f;
     // Start is called before the first frame update
-    private float cameraDistance;
-
-
 
     //TODO get this from a level class
     public Vector2 levelPositionUL = new Vector2(); // Level position upper left
@@ -21,9 +18,7 @@ public class Inputmanager : MonoBehaviour
     void Start()
     {
         Cursor.lockState = CursorLockMode.Confined;
-        cameraDistance = -Camera.main.transform.position.z;
         Debug.unityLogger.Log("Camera Distance: " + -Camera.main.transform.position.z);
-
     }
 
     void Update()
@@ -33,12 +28,38 @@ public class Inputmanager : MonoBehaviour
 
     void MoveCamera()
     {
+        // init new coords for moving the camera
         float moveX = Camera.main.transform.position.x;
         float moveY = Camera.main.transform.position.y;
 
+        //Get mouse position
         float xPos = Input.mousePosition.x;
         float yPos = Input.mousePosition.y;
+
         // Input.mousePresent
+
+        //Mousewheel input
+
+        float mouseScroll = Input.GetAxis("Mouse ScrollWheel");
+
+        Camera.main.orthographicSize -= mouseScroll;
+
+        if (!IsNotOnBottom())
+        {
+            if (!IsNotOnTop())
+                Camera.main.orthographicSize += mouseScroll;
+            moveY -= mouseScroll < 0 ? mouseScroll : 0;
+        }
+
+        if (!IsNotOnTop())
+        {
+            if (!IsNotOnBottom())
+                Camera.main.orthographicSize += mouseScroll;
+            moveY += mouseScroll < 0 ? mouseScroll : 0;
+        }
+
+        // move Input, mouse and keyboard
+
         if (Input.GetAxisRaw("Horizontal") < 0f || xPos >= 0 && xPos <= mouseDetect) //A
         {
             moveX -= cameraMoveSpeed;
@@ -57,44 +78,37 @@ public class Inputmanager : MonoBehaviour
                 //TODO Load level from other side
             }
         }
-
+        //W
         if (IsNotOnTop() && ((Input.GetAxisRaw("Vertical") > 0f || yPos <= Screen.height && yPos >= Screen.height - mouseDetect)))
         {
-            if ((levelPositionUL.y - Camera.main.transform.position.y + Camera.main.orthographicSize) < cameraMoveSpeed)
+            Debug.Log((levelPositionUL.y - Camera.main.transform.position.y + Camera.main.orthographicSize).ToString());
+            if ((levelPositionUL.y - Camera.main.transform.position.y + Camera.main.orthographicSize) <= cameraMoveSpeed)
             {
                 moveY += levelPositionUL.y - Camera.main.transform.position.y + Camera.main.orthographicSize;
+                Debug.Log(levelPositionUL.y - Camera.main.transform.position.y + Camera.main.orthographicSize);
             }
             else
                 moveY += cameraMoveSpeed;
+
         }
+        //S 
         else if (IsNotOnBottom() && (Input.GetAxisRaw("Vertical") < 0f || yPos >= 0 && yPos <= mouseDetect))
         {
             moveY -= cameraMoveSpeed;
         }
 
-        float mouseScroll = Input.GetAxis("Mouse ScrollWheel");
-
-        if (Camera.main.transform.position.y - Camera.main.orthographicSize <
-         levelPositionBR.y)
-            Camera.main.orthographicSize -= mouseScroll > 0 ? mouseScroll : 0;
-        else if (Camera.main.transform.position.y + Camera.main.orthographicSize < levelPositionUL.y)
-            Camera.main.orthographicSize -= mouseScroll < 0 ? mouseScroll : 0;
-
-        Vector3 newPosition = new Vector3(moveX, moveY, -cameraDistance);
+        //Save new Position in var
+        Vector3 newPosition = new Vector3(moveX, moveY, Camera.main.transform.position.z);
 
         Camera.main.transform.position = newPosition;
     }
 
     private bool IsNotOnBottom()
     {
-        if (Camera.main.transform.position.y - Camera.main.orthographicSize > levelPositionBR.y)
-            return true;
-        return false;
+        return Camera.main.transform.position.y - Camera.main.orthographicSize > levelPositionBR.y;
     }
     private bool IsNotOnTop()
     {
-        if (Camera.main.transform.position.y + Camera.main.orthographicSize <= levelPositionUL.y)
-            return true;
-        return false;
+        return Camera.main.transform.position.y + Camera.main.orthographicSize < levelPositionUL.y;
     }
 }
