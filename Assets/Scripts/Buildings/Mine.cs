67b0119466493fee9a0527fbc4aecbcd78edc8cc
Theo Stötzer <geometheo@yahoo.de>
2020-11-30 11:01:16 +0100

    Created 2 building prefabs
    - added prefabs for coal mine and base building
    - added scripts, already bound to building objects

---
+++ Assets/Scripts/Buildings/Mine.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mine : MonoBehaviour {
+    // Start is called before the first frame update
+
+    [Range(-500, 500)][SerializeField] int hp;
+    [SerializeField] int fogOfWar;
+    private bool down = true;
+
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (hp == -500) down = false;
+        if (hp == 500) down = true;
+
+        if (down) hp--;
+        else hp++;
+    }
+
+    //private void 
+
+}
