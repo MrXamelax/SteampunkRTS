83dfac2a784dcb6eb97355b628a8a4cda5c93968
Theo Stötzer <geometheo@yahoo.de>
2020-11-30 13:47:58 +0100

    Coal mine trigger detection
    - added trigger detection for coal mines

--- Assets/Scripts/Buildings/Mine.cs
+++ Assets/Scripts/Buildings/Mine.cs
@@ -1,13 +1,18 @@
-﻿using System.Collections;
+﻿using Photon.Pun;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Mine : MonoBehaviour {
     // Start is called before the first frame update
 
-    [Range(-500, 500)][SerializeField] int hp;
+    // Everything below 0 = own mine
+    // Everything above 0 = enemey mine
+    [Range(-500, 500)][SerializeField] int ownershipPoints;
     [SerializeField] int fogOfWar;
     private bool down = true;
+    //private int unitsMaster = 0;
+    //private int unitsClient = 0;
 
     void Start() {
 
@@ -16,13 +21,27 @@ public class Mine : MonoBehaviour {
     // Update is called once per frame
     void Update() {
 
-        if (hp == -500) down = false;
-        if (hp == 500) down = true;
+        if (ownershipPoints == -500) down = false;
+        if (ownershipPoints == 500) down = true;
 
-        if (down) hp--;
-        else hp++;
+        if (down) ownershipPoints--;
+        else ownershipPoints++;
     }
 
+    //[PunRPC]
     //private void 
-
+    
+    private void OnTriggerEnter2D(Collider2D col) {
+        Debug.Log("Kollision, mein Freund!");
+        //if (col.GetComponent<PhotonView>().Owner) Debug.LogWarning("Meins!");
+        //else Debug.LogWarning("Nicht meins!");
+    }
+    
+    /*
+    private void OnTriggerEnter(Collider col) {
+        Debug.Log("Kollision, mein Freund!");
+       // if (col.GetComponent<PhotonView>().Owner.IsMasterClient) Debug.LogWarning("Meins!");
+       // else Debug.LogWarning("Nicht meins!");
+    }
+    */
 }
