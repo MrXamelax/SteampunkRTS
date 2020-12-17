e72c52087fc87409df1326d2e383a6e788f6f12c
Hasenknopf <wikinger29@googlemail.com>
2020-12-17 21:49:08 +0100

    added UI mainmenu, new Scenes (Settings,Endscreen)

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("Mainmenu");
+    }
+
+    public void OpenSettings()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Launcher");
+    }
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
