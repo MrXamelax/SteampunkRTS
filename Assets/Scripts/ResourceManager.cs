018379d0f7479c2ff099afc51b047a949573c963
Theo Stötzer <geometheo@yahoo.de>
2020-12-18 06:32:14 +0100

    Step 1
    - things will follow

--- Assets/Scripts/ResourceManager.cs
+++ Assets/Scripts/ResourceManager.cs
@@ -3,17 +3,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ResourceManager  {
+public class ResourceManager : MonoBehaviour {
 
     public static ResourceManager Instance; //Singleton. To be accessed from everywhere
-
-    /*
-    textM.text = "Coal Master: " + ResourceManager.Instance.getCoal('m');
-    textC.text = "Coal Client: " + ResourceManager.Instance.getCoal('c');
-
-    [SerializeField] Text textM;
-    [SerializeField] Text textC;
-    */
 
     private int coalMaster = 2500;
     private int coalClient = 2500;
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,17 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class UIManager : MonoBehaviour
-{
+public class UIManager : MonoBehaviour {
     [SerializeField] GameObject buildingMenu;
     [SerializeField] GameObject unitMenu;
     [SerializeField] GameObject breedMenu;
     [SerializeField] GameObject factoryMenu;
     [SerializeField] GameObject mineMenu;
+
+    [SerializeField] Text coalAmount;
+
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         buildingMenu.SetActive(false);
         unitMenu.SetActive(false);
         breedMenu.SetActive(false);
@@ -20,86 +22,66 @@ public class UIManager : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        coalAmount.text = "C: " + ResourceManager.Instance.getCoal('m');
     }
 
-    public void openBuildingMenu()
-    {
-        if (!buildingMenu.activeSelf)
-        {
-            buildingMenu.SetActive(true); 
+    public void openBuildingMenu() {
+        if (!buildingMenu.activeSelf) {
+            buildingMenu.SetActive(true);
             mineMenu.SetActive(false);
             breedMenu.SetActive(false);
             factoryMenu.SetActive(false);
             unitMenu.SetActive(false);
-        } else
-        {
+        } else {
             buildingMenu.SetActive(false);
         }
     }
 
-    public void openUnitMenu()
-    {
-        if (!unitMenu.activeSelf)
-        {
+    public void openUnitMenu() {
+        if (!unitMenu.activeSelf) {
             unitMenu.SetActive(true);
             buildingMenu.SetActive(false);
             breedMenu.SetActive(false);
             factoryMenu.SetActive(false);
             mineMenu.SetActive(false);
-        }
-        else
-        {
+        } else {
             unitMenu.SetActive(false);
         }
     }
 
-    public void openMineMenu()
-    {
-        if (!mineMenu.activeSelf)
-        {
+    public void openMineMenu() {
+        if (!mineMenu.activeSelf) {
             mineMenu.SetActive(true);
             buildingMenu.SetActive(false);
             breedMenu.SetActive(false);
             factoryMenu.SetActive(false);
             unitMenu.SetActive(false);
-        }
-        else
-        {
+        } else {
             mineMenu.SetActive(false);
         }
     }
 
-    public void openBreedMenu()
-    {
-        if (!breedMenu.activeSelf)
-        {
+    public void openBreedMenu() {
+        if (!breedMenu.activeSelf) {
             mineMenu.SetActive(false);
             buildingMenu.SetActive(false);
             breedMenu.SetActive(true);
             factoryMenu.SetActive(false);
             unitMenu.SetActive(false);
-        }
-        else
-        {
+        } else {
             breedMenu.SetActive(false);
         }
     }
 
-    public void openFactoryMenu()
-    {
-        if (!factoryMenu.activeSelf)
-        {
+    public void openFactoryMenu() {
+        if (!factoryMenu.activeSelf) {
             mineMenu.SetActive(false);
             buildingMenu.SetActive(false);
             breedMenu.SetActive(false);
             factoryMenu.SetActive(true);
             unitMenu.SetActive(false);
-        }
-        else
-        {
+        } else {
             factoryMenu.SetActive(false);
         }
     }
