473e30b38f00c3cf4f9150d5a4efcf039ada965e
Richard <yegac000@gmail.com>
2021-02-15 20:41:48 +0100

    fixed win/ lose

--- Assets/Scripts/Buildings/Base.cs
+++ Assets/Scripts/Buildings/Base.cs
@@ -7,17 +7,12 @@ public class Base : MonoBehaviour
 {
     private PhotonView pview;
 
-    bool isMine;
     private void Start()
     {
         pview = GetComponent<PhotonView>();
-        isMine = pview.IsMine;
-
     }
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.V) && !pview.IsMine) GetComponent<PhotonView>().RPC("takeDMG", RpcTarget.All, 5);
     }
-
-    private void OnDestroy() => GameManager.Instance.GameOver(isMine);
 }
